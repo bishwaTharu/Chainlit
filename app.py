f26f260617d1c767b4f48663d2a4e569d80be5ec
bishwaTharu <bishwanaththaru34@gmail.com>
2023-07-18 14:26:08 +0545

    Duckduck go search Integration

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
 import os
 import chainlit as cl
-import faiss
 from dotenv import load_dotenv, find_dotenv
 from langchain import HuggingFaceHub
 from huggingface_hub import login
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from langchain.experimental import AutoGPT
 from langchain.agents import Tool
-from langchain.docstore import InMemoryDocstore
 from langchain.tools import DuckDuckGoSearchRun
-from langchain.tools.file_management.write import WriteFileTool
-from langchain.tools.file_management.read import ReadFileTool
-from langchain.embeddings import HuggingFaceHubEmbeddings
-from langchain.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from langchain.agents import AgentType
 
 
 # authentication
@@ -32,38 +29,31 @@ llm = HuggingFaceHub(
 
 
 search = DuckDuckGoSearchRun()
+# memory = ConversationBufferMemory(memory_key="chat_history")
+
 
 tools = [
     Tool(
         name="DuckDuckGo Search",
         func=search.run,
-        description="useful for when you need to answer questions about current events. You should ask targeted questions",
-    ),
-    WriteFileTool(),
-    ReadFileTool(),
+        description="useful for when you need to answer questions about current events or the current state of the world",
+    )
 ]
-
-embeddings_model = HuggingFaceHubEmbeddings()
-embedding_size = 768
-index = faiss.IndexFlatL2(embedding_size)
 
 
 @cl.langchain_factory(use_async=False)
 def agent():
-    vectorstore = FAISS(embeddings_model.embed_query, index, InMemoryDocstore({}), {})
-    callbacks = [cl.LangchainCallbackHandler()]
-    agent = AutoGPT.from_llm_and_tools(
-        ai_name="Jarvis",
-        ai_role="Assistant",
-        tools=tools,
-        llm=llm,
-        memory=vectorstore.as_retriever(),
-
+    agent_chain = initialize_agent(
+        tools,
+        llm,
+        agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION,
+        verbose=True,
+        # memory=memory,
+        handle_parsing_errors="Check your output and make sure it conforms!",
     )
+
     # Set verbose to be true
-    agent.chain.verbose = True
-    agent.chain.callbacks = callbacks
-    return agent
+    return agent_chain
 
 
 @cl.langchain_run
